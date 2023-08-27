e6046092d346013ce100b36b12d817579c950c11
Omar Augusto Bautista Mora <oabm77@gmail.com>
2023-08-27 18:52:54 -0500

    05 Temas - Temas #1

--- src/App.js
+++ src/App.js
@@ -3,14 +3,18 @@ import React from "react";
 import Container from "./Components/Container";
 import Header from "./Components/Header";
 import GlobalStyle from "./GlobalStyle";
+import { ThemeProvider } from "styled-components";
+import { temaClaro, temaOscuro } from "./Components/UI/temas";
+
+console.log(temaClaro, temaOscuro)
 
 function App() {
   return (
-    <>
+    <ThemeProvider theme={ temaOscuro }>
       <GlobalStyle />
       <Header />
       <Container />
-    </>
+    </ThemeProvider>
   );
 }
 
