4f051c330e829f61de6a33cbc610514fa33cba21
Omar Augusto Bautista Mora <oabm77@gmail.com>
2023-08-27 07:18:35 -0500

    03 Refactorización - Media Query

--- src/Components/Container/index.jsx
+++ src/Components/Container/index.jsx
@@ -9,19 +9,23 @@ const StyledContainer = styled.div`
   padding: 0px 15vw;
 `
 
-const StiledContent = styled.section`
+const StyledContent = styled.section`
   display: flex;
   flex-direction: row;
   justify-content: space-between;
+  @media (max-width: 800px) {
+    flex-direction: column;
+  };
 `
 
 const Container = () => {
   return (
     <StyledContainer>
       <Title>Smart Bank</Title>
-      <StiledContent>
+      <StyledContent>
         <Account />
-      </StiledContent>
+        <Account />
+      </StyledContent>
     </StyledContainer>
   );
 };
