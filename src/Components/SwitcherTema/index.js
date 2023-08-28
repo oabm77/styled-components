84d94163d0e40a38008eb88ce915a278b032a391
Omar Augusto Bautista Mora <oabm77@gmail.com>
2023-08-27 19:09:43 -0500

    05 Temas - Cambio de tema

--- src/Components/SwitcherTema/index.js
+++ src/Components/SwitcherTema/index.js
@@ -3,7 +3,7 @@ import themeOn from "../../assets/images/themeOn.svg"
 import themeOff from "../../assets/images/themeOff.svg"
 import { Icono } from "../UI"
 
-export default (tema) => {
+export default ({tema}) => {
     const claro = <Icono src={themeOn} alt="Tema Claro" />
     const oscuro = <Icono src={themeOff} alt="Tema Oscuro" />
 
